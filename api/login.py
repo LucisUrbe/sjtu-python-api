f47f3b55d4b72cbf904909464aa1ed1745303485
GilbertJin <17175403+GilbertJin@users.noreply.github.com>
2022-04-16 16:32:37 +0800

    Updated working login process for "i", ready for getting class table and other cookies.

--- api/login.py
+++ api/login.py
@@ -1,16 +1,15 @@
 import requests
-import matplotlib.pyplot as plt
-from io import BytesIO
 
 import api.form as form
 import api.captcha as captcha
 
 
-def login(jaccount_name: str, jaccount_password: str) -> dict:
+def login(jaccount_name: str, jaccount_password: str) -> requests.Session:
     try:
         if jaccount_name == '' or jaccount_password == '':
             raise ValueError('INVALID USER NAME OR PASSWORD.')
         session = requests.Session()
+        session.get('https://jaccount.sjtu.edu.cn/oauth2/logout')
         url = 'https://i.sjtu.edu.cn/jaccountlogin'
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15',
@@ -18,13 +17,14 @@ def login(jaccount_name: str, jaccount_password: str) -> dict:
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
         } # initial headers
         response = session.get(url, headers=headers)
+        url_fix = response.url
 
-        cookies = ''
+        cookie = ''
         for key, value in response.cookies.items():
-            cookies += key + '=' + value + ';'
+            cookie += key + '=' + value + ';'
         
         headers.update({
-            'Cookie': cookies,
+            'Cookie': cookie,
             'Origin': 'https://jaccount.sjtu.edu.cn',
         }) # headers with jaccount session id
         
@@ -35,24 +35,31 @@ def login(jaccount_name: str, jaccount_password: str) -> dict:
         })
 
         for _ in range(1, 10): # try maximum 10 times
-            jaccount_captcha = captcha.captcha(submit['uuid'], cookies).strip().replace(' ','')
+            jaccount_captcha = captcha.captcha(submit['uuid'], cookie).strip().replace(' ','')
 
             submit.update({
-                'captcha': jaccount_captcha
+                'captcha': jaccount_captcha,
             })
 
             url = 'https://jaccount.sjtu.edu.cn/jaccount/ulogin?'
             for key, value in submit.items():
                 url += key + '=' + value + '&'
-            response = session.post(url, headers=headers)
+            response = session.post(url, headers=headers, allow_redirects=False) # return URL from ulogin is not true
 
-            cookies = session.cookies.get_dict() # dict
-            if (cookies.get('JAAuthCookie') is not None):
-                return cookies
+            cookie: dict = session.cookies.get_dict()
+            if (cookie.get('JAAuthCookie') is not None):
+                cookie_fix = ''
+                for key, value in cookie.items():
+                    cookie_fix += key + '=' + value + ';'
+                headers.update({
+                    'Cookie': cookie_fix
+                })
+                response = session.get(url_fix, headers=headers) # get the right address
+                return session
             else:
-                cookies = ''
+                cookie = ''
                 for key, value in response.cookies.items():
-                    cookies += key + '=' + value + ';'
+                    cookie += key + '=' + value + ';'
                 if '&err=' in response.url:
                     if '&err=1' in response.url:
                         print('[WARN/LOGIN] WRONG CAPTCHA SOLUTION, RETRYING...')
@@ -73,57 +80,63 @@ def login(jaccount_name: str, jaccount_password: str) -> dict:
                         quit(-5)
                     elif '&err=6' in response.url:
                         print('[ERROR/LOGIN] YOU HAS CHANGED YOUR PASSWORD. CURRENT SESSION REQUIRE YOU TO RETRY LOGIN.')
-                        return {}
+                        return requests.Session() #
                     elif '&err=7' in response.url:
                         print('[ERROR/LOGIN] YOUR ACCOUNT IS CURRENTLY NOT LOG-ABLE.')
                         quit(-7)
                     elif '&err=8' in response.url:
                         print('[ERROR/LOGIN] LOGIN SESSION IS STALE. PLEASE RETRY.')
-                        return {} #
+                        return requests.Session() #
                     elif '&err=9' in response.url:
                         print('[ERROR/LOGIN] QR CODE IS STALE. PLEASE REFRESH.')
-                        return {} #
+                        return requests.Session() #
                     elif '&err=10' in response.url:
                         print('[ERROR/LOGIN] SJTU INFOPLUS TASKCENTER LOGIN IS STALE. RETRY LOGGIN IN TO THE APP.')
-                        return {} #
+                        return requests.Session() #
                     elif '&err=11' in response.url:
                         print('[ERROR/LOGIN] PLEASE SCAN QR CODE BY WECHAT OR SJTU INFOPLUS TASKCENTER.')
-                        return {} #
+                        return requests.Session() #
                     elif '&err=12' in response.url:
                         print('[ERROR/LOGIN] EXCEPTION WHILE FETCHING WECHAT LOGIN. PLEASE RETRY.')
-                        return {} #
+                        return requests.Session() #
                     elif '&err=13' in response.url:
                         print('[ERROR/LOGIN] NO JACCOUNT BINDED TO WECHAT. PLEASE LOG IN BY USER NAME AND PASSWORD.')
                         quit(-13)
                     else:
                         print('[ERROR/LOGIN] UNDEFINED ERROR.')
                 else:
-                    print('[WARN/LOGIN] WEAK NETWORK CONNECTION, RETRYING...', response.url)
+                    print('[WARN/LOGIN] WEAK NETWORK CONNECTION, RETRYING...')
                     
         
         print('[WARN/LOGIN] 10 TIMES ERROR ON CAPTCHA')
-        cookies = session.cookies.get_dict()
-        while (cookies.get('JAAuthCookie') is None):
-            print('[WARN/LOGIN] YOU MUST RETRY CAPTCHA MANUALLY')
+        return requests.Session()
+        # cookies: dict = session.cookies.get_dict()
+        # while (cookies.get('JAAuthCookie') is None):
+        #     print('[WARN/LOGIN] YOU MUST RETRY CAPTCHA MANUALLY')
 
-            from api.captcha import refresh_captcha
-            image = refresh_captcha(submit['uuid'], cookies)
-            plt.imshow(plt.imread(BytesIO(image)))
-            plt.imshow()
+        #     Cookies: str = ''
+        #     for key, value in response.cookies.items():
+        #         Cookies += key + '=' + value + ';'
+        #     from api.captcha import refresh_captcha
+        #     image = refresh_captcha(submit['uuid'], Cookies)
+        #     from io import BytesIO
+        #     from PIL import Image
+        #     image = Image.open(BytesIO(image))
+        #     image.show()
 
-            jaccount_captcha = input('<CAPTCHA>:')
-            submit.update({
-                'captcha': jaccount_captcha
-            })
+        #     jaccount_captcha = input('<CAPTCHA>:')
+        #     submit.update({
+        #         'captcha': jaccount_captcha
+        #     })
 
-            url = 'https://jaccount.sjtu.edu.cn/jaccount/ulogin?'
-            for key, value in submit.items():
-                url += key + '=' + value + '&'
-            response = session.post(url, headers=headers)
-            if '&err=' in response.url:
-                print('[ERROR/LOGIN] AN ERROR OCCURRED WHILE LOGGIN IN. CONSIDER A WRONG USER NAME OR PASSWORD, OR A WEAK NETWORK QUALITY.')
-                return {} #
-            cookies = session.cookies.get_dict() # dict
-        return cookies
+        #     url = 'https://jaccount.sjtu.edu.cn/jaccount/ulogin?'
+        #     for key, value in submit.items():
+        #         url += key + '=' + value + '&'
+        #     response = session.post(url, headers=headers)
+        #     if '&err=' in response.url:
+        #         print('[ERROR/LOGIN] AN ERROR OCCURRED WHILE LOGGIN IN. CONSIDER A WRONG USER NAME OR PASSWORD, OR A WEAK NETWORK QUALITY.')
+        #         return {} #
+        #     cookies = session.cookies.get_dict() # dict
+        # return cookies
     except Exception as error:
         print('[ERROR/LOGIN]', error)
--- demo.py
+++ demo.py
@@ -9,12 +9,12 @@ def main() -> None:
     print('SJTU JACCOUNT SSO API DEMO')
     jaccount_name = input('<JACCOUNT> ')
     jaccount_password = getpass.getpass('<PASSWORD> ')
-    cookies = entry(jaccount_name, jaccount_password)
-    if cookies == {}:
+    session = entry(jaccount_name, jaccount_password)
+    if session.cookies.items() == []:
         print('Error occurred while trying to log in. Did you input an invalid user name or password?')
     else:
         print('Success! Your secret cookie key is:')
-        print(cookies.get('JAAuthCookie'))
+        print(session.cookies.get('JAAuthCookie'))
         print('Do not tell anyone!')
 
 if __name__ == '__main__':
